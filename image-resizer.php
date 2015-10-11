271cfeed02676f3cd6bfad1275b09fc52a46506c
Tzvetan Koshutanski <ceckobecko@gmail.com>
2015-10-12 00:32:35 +0300

    image resizer

--- image-resizer.php
+++ image-resizer.php
@@ -1,16 +1,42 @@
 <?php
+//require_once(Imagick::class);
+define('PHOTO_DIR', '/home/tsvetan/Documents/pic_folders/');
+define('THUMBNAILS_DIRECTORY', __DIR__ . '/copy_dir/');
+define('THUMBNAIL_SIZE_X', 100);
+define('THUMBNAIL_SIZE_Y', 100);
 
-define('PHOTO_DIR' , '/home/tsvetan/Documents/Pic folders');
-define('COPY_DIR', '');
-$thumbnails_size = ['x' => 100, 'y' => 100];
 
-function loadImage(){};
-function resizeImage(){};
-function saveImage(){};
-
-function loadRecursively(){
-    $jpgs = glob(PHOTO_DIR . '/' . '*.{jpg}');
-    print json_encode($jpgs);
+function makeThumbnails()
+{
+    $directoryIterator = new RecursiveDirectoryIterator(PHOTO_DIR);
+    $iterator = new RecursiveIteratorIterator($directoryIterator);
+    $regexIterator = new RegexIterator($iterator, '/^.+\.jpg/i');
+    /**
+     * @var SplFileInfo $image
+     */
+    foreach ($regexIterator as $image) {
+        try {
+            print "Creating thumbnail for image: {$image->getPathname()}\r\n";
+            $imageName = $image->getPathname();
+            $imageHash = crc32($imageName);
+            $imagick = new Imagick();
+            $imagick->readImage($imageName);
+            $imagick->thumbnailImage(THUMBNAIL_SIZE_X, THUMBNAIL_SIZE_Y);
+            $imagick->setImageFormat('jpg');
+            $imagick->writeImage(getThumbnailsDirectory() . "$imageHash.jpg");
+        } catch (Exception $e) {
+            print "Error: Not able to create thumbnail: {$e->getMessage()}\r\n";
+        }
+    }
 }
 
-loadRecursively();
+function getThumbnailsDirectory()
+{
+    $thumbnailsDirectory = THUMBNAILS_DIRECTORY;
+    if (isset($argv[1])) {
+        $thumbnailsDirectory = $argv[1];
+    }
+    return $thumbnailsDirectory;
+}
+
+makeThumbnails();
